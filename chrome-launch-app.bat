2ca336200df0397b4508eb20d89c83d2a411a6f9
pemn <paulo.ernesto@mail.com>
2016-07-11 09:42:31 -0300

    Add files via upload

---
+++ chrome-launch-app.bat
@@ -0,0 +1,8 @@
+:: Launch the current folder as a Chrome APP
+@echo off
+
+:: find the path to chrome
+for /f "TOKENS=2 delims=:" %%I in ('reg query "HKLM\Software\Microsoft\Windows\CurrentVersion\App Paths\Chrome.exe" /ve') do set chrome_exe=%systemdrive%%%I
+
+:: start chrome with this app
+@"%chrome_exe%" --load-and-launch-app=%~dp0
